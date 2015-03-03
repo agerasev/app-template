2c371b89ef7c08ec692cf6eea0cbafaff0f3c2cc
Alexey Gerasev <alexey.gerasev@gmail.com>
2015-03-03 15:07:11 +0600

    makefile generator

--- build-info.mk
+++ build-info.mk
@@ -2,28 +2,16 @@
 _DESKTOP_LIBS = -lSDL2 -lGLEW -lGL
 _DESKTOP_CFLAGS =
 _DESKTOP_CXXFLAGS = 
-_DESKTOP_SOURCES = \
-	source/media/desktop.c
+_DESKTOP_SOURCES = 
 
 # Android
 _ANDROID_LIBS = -llog -landroid  -lEGL -lGLESv2
 _ANDROID_CFLAGS =
 _ANDROID_CXXFLAGS = 
-_ANDROID_SOURCES = \
-	source/media/android.c
+_ANDROID_SOURCES = 
 
 # Common
 _CFLAGS = 
 _CXXFLAGS = -std=c++11
-_HEADERS = \
-	source/media/media.h \
-	source/graphics/graphics.h \
-	source/vec.hpp \
-	source/circle.hpp \
-	source/pool.hpp
-
-_SOURCES = \
-	source/graphics/graphics.c \
-	source/pool.cpp \
-	source/circle.cpp \
-	source/main.cpp
+_SOURCES = 
+_HEADERS = 
